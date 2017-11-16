deb17045483992bfeb2cd8b26d0c0e6ad09f9518
darrenjennings <dmjenn02@gmail.com>
2017-11-15 20:19:47 -0800

    Updated copy.

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
 const Home = () =>
   <div className="outerblock">
@@ -9,70 +9,50 @@ const Home = () =>
       </div>
       <hr />
       <p>
-        Brittany is a classically trained cellist, enthusiastic about sharing
-        the violoncello through giving lessons and collaborating with other
-        musicians for weddings and recordings. Her imaginative improvisation is
-        a wonderful, versatile addition to any musical group or occassion.
+        Brittany is a classically trained cellist, enthusiastic about sharing the violoncello through giving lessons and
+        collaborating with other musicians for weddings and recordings. Her imaginative improvisation is a wonderful,
+        versatile addition to any musical group or occassion.
       </p>
       <blockquote>
         <li>
-          Attended Western Kentucky University with the Blanche & Austin Duckett
-          Strings Music Scholarship and the Jerry Baker Scholarship. Received
-          degrees in both music and visual arts.
+          Playing style influenced by Irish and American Folk music. Grew up playing Old-Time music with my dad, Eric
+          Jarboe, who rocks the hammer dulcimer!
         </li>
+        <li>Prefers creating and playing by ear and can also read music.</li>
+        <li>Experienced in giving lessons to various ages ranging from beginner to intermediate.</li>
         <li>
-          Experienced in giving lessons to various ages ranging from beginner to
-          intermediate.
-        </li>
-        <li>
-          Recorded and performed with several bands/artists including:{" "}
-          <a
-            target="_blank"
-            href="https://brooksritter.bandcamp.com/track/prodigal-feet"
-          >
+          Recorded and performed with several bands/artists including:{' '}
+          <a target="_blank" href="https://brooksritter.bandcamp.com/track/prodigal-feet">
             Brooks Ritter
-          </a>,{" "}
-          <a
-            target="_blank"
-            href="https://soundcloud.com/harlowsmonkeyssf/everyone-should-ride-bikes"
-          >
+          </a>,
+          <a target="_blank" href="https://soundcloud.com/harlowsmonkeyssf/everyone-should-ride-bikes">
             Harlow's Monkeys
-          </a>, and {" "}
-          <a
-            target="_blank"
-            href="https://theparadeschedule.bandcamp.com/track/wyoming"
-          >
+          </a>, and {' '}
+          <a target="_blank" href="https://theparadeschedule.bandcamp.com/track/wyoming">
             Matt Kinder of the Parade Schedule
           </a>.
         </li>
       </blockquote>
-      Cello Philosophy - You do not "arrive" one day playing your cello
-      perfectly and knowing all of its nuances. Being a musician and a cellist
-      is a journey of imaginatively exploring a complex and beautiful
-      instrument. The cello is an instrument that gives joy to the musicians and
-      listeners alike.
+      🎻 Cello Philosophy - You do not "arrive" one day playing your cello perfectly and knowing all of its nuances.
+      Being a musician and a cellist is a journey of imaginatively exploring a complex and beautiful instrument. The
+      cello is an instrument that gives joy to the musicians and listeners alike.
       <hr />
       <i>
-        <div style={{ textAlign: "right" }}>
-          "The spirit of the music is the only thing which can rightfully
-          dictate physical action on the cello."
+        <div style={{ textAlign: 'right' }}>
+          "The spirit of the music is the only thing which can rightfully dictate physical action on the cello."
           <br /> William Pleeth, Cellist and Teacher
         </div>
       </i>
     </div>
     <hr />
-    <audio controls autoPlay style={{ width: "100%" }}>
-      <source
-        src="https://s3-us-west-2.amazonaws.com/brittany-jennings/brittany321.mp3"
-        type="audio/mpeg"
-      />
+    <audio controls autoPlay style={{ width: '100%' }}>
+      <source src="https://s3-us-west-2.amazonaws.com/brittany-jennings/brittany321.mp3" type="audio/mpeg" />
       Your browser does not support the audio element.
     </audio>
-    <span style={{ fontStyle: "italic" }}>
-      Brittany's recording of J.S. Bach - Cello Suite no. 1 in G major: II.
-      Allemande
+    <span style={{ fontStyle: 'italic' }}>
+      Brittany's recording of J.S. Bach - Cello Suite no. 1 in G major: II. Allemande
     </span>
-    <div style={{ textAlign: "center", margin: "0 auto", padding: "20px" }}>
+    <div style={{ textAlign: 'center', margin: '0 auto', padding: '20px' }}>
       <Link href="/contact">
         <a>
           <img height="30px" src="/static/svg/contact.svg" /> Contact Me!
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Header from "../components/Header";
-import Home from "../components/Home";
+import React from 'react';
+import Header from '../components/Header';
+import Home from '../components/Home';
 
 export default () =>
-  <div>
-    <Header />
-    <div className="body-container">
-      <Home />
-    </div>
-  </div>;
+	<div>
+		<Header />
+		<div className="body-container">
+			<Home />
+		</div>
+	</div>;
